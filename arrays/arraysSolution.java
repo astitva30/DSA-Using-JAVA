6199387e8d2a7d593cc17a8020acd18b3238c494
Astitva <deshpandeastitva@gmail.com>
2022-05-28 11:46:05 +0530

    first commit

---
+++ arrays/arraysSolution.java
@@ -0,0 +1,11 @@
+class arraysSolution{
+    public static void main(String[] args){
+        int[] a1= new int[6];
+        for(int i=0;i<a1.length;i++){
+            a1[i]=i+1;
+        }
+        for(int j=0;j<a1.length;j++){
+            System.out.println(a1[j]);
+        }
+    }
+}
